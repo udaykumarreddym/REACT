643554c8289d24971fc34187a644135e4376a0d4
Uday Kumar Reddy <udaykumarreddy939@gmail.com>
2025-09-07 23:36:58 +0530

    update

--- App.js
+++ App.js
@@ -1,28 +1,17 @@
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading", xyz: "abc" },
-//   "Hello World from React!"
-// );
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-// console.log(heading); // Object
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-
-// root.render(heading); // Converts Object to HTML and shows on the screen
-
-const child = React.createElement("div", { id: "child" }, [
-  React.createElement("h1", { id: "heading1" }, "I am h1 tag"),
-  React.createElement("h2", { id: "heading2" }, "I am h2 tag"),
-]);
-const child1 = React.createElement("div", { id: "child1" }, [
-  React.createElement("h1", { id: "heading3" }, "I am h1 tag"),
-  React.createElement("h2", { id: "heading4" }, "I am h2 tag"),
+const child = React.createElement("div", { id: "child", key: "child" }, [
+  React.createElement("h1", { id: "heading1", key: "h1-1" }, "I am h1 tag"),
+  React.createElement("h2", { id: "heading2", key: "h2-1" }, "I am h2 tag"),
 ]);
 
-const parent = React.createElement("div", { id: "parent" }, [child, child1 ]);
+const child1 = React.createElement("div", { id: "child1", key: "child1" }, [
+  React.createElement("h1", { id: "heading3", key: "h1-2" }, "I am h1 tag"),
+  React.createElement("h2", { id: "heading4", key: "h2-2" }, "I am h2 tag"),
+]);
 
-console.log(parent);
+const parent = React.createElement("div", { id: "parent" }, [child, child1]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
 root.render(parent);
