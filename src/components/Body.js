5547e3b36fe4dc707408da29976ceea0fcc69835
Uday Kumar Reddy <udaykumarreddy939@gmail.com>
2025-12-20 00:31:12 +0530

    added filter btn using React useState hook

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,25 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
+  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredData = resList.filter((res) => res.avgRating > 4.2);
+            setListOfRestaurants(filteredData);
+            console.log("Filtered Data:", filteredData);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
       <div className="res-container">
-        {resList.map((restaurant) => (
+        {listOfRestaurants.map((restaurant) => (
           <RestaurantCard key={restaurant.id} resData={restaurant} />
         ))}
       </div>
