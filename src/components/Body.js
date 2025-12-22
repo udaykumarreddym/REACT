6c7a84d124aa03d48a461ba6f7d4289a7bc9dfc6
Uday Kumar Reddy <udaykumarreddy939@gmail.com>
2025-12-23 00:29:48 +0530

    Updated with Swiggy API

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,27 +1,45 @@
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockData";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Body = () => {
-  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.3605586&lng=78.478036&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    console.log(json);
+    setListOfRestaurants(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
   return (
     <div className="body">
       <div className="filter">
         <button
           className="filter-btn"
           onClick={() => {
-            const filteredData = resList.filter((res) => res.avgRating > 4.2);
+            const filteredData = listOfRestaurants.filter(
+              (res) => res?.info?.avgRating > 4.0
+            );
             setListOfRestaurants(filteredData);
-            console.log("Filtered Data:", filteredData);
           }}
         >
           Top Rated Restaurants
         </button>
       </div>
       <div className="res-container">
-        {listOfRestaurants.map((restaurant) => (
-          <RestaurantCard key={restaurant.id} resData={restaurant} />
-        ))}
+        {listOfRestaurants
+          ?.filter((restaurant) => restaurant?.info?.id)
+          .map((restaurant) => (
+            <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+          ))}
       </div>
     </div>
   );
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,13 +1,23 @@
-const RestaurantCard = (props) => {
-  const { resData } = props;
-  const { name, cuisines, avgRating, costForTwo, image } = resData;
+const RestaurantCard = ({ resData }) => {
+  const {
+    name,
+    cuisines = [],
+    avgRating,
+    costForTwo,
+    cloudinaryImageId,
+  } = resData.info;
+
+  const image =
+    "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+    cloudinaryImageId;
+
   return (
     <div className="res-card">
-      <img className="res-logo" src={image} />
+      <img className="res-logo" src={image} alt={name} />
       <h3>{name}</h3>
       <h4>{cuisines.join(", ")}</h4>
       <h4>{avgRating}</h4>
-      <h4>₹ {costForTwo / 100} FOR TWO</h4>
+      <h4>{costForTwo}</h4>
     </div>
   );
 };
